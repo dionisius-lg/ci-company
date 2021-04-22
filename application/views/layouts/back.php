f0be2b3bd5ab0526a03202d12b9badd64026aab0
ridhorambu93 <ridhodavybones@gmail.com>
2021-04-23 03:30:05 +0700

    add active menu in admin/back

--- application/views/layouts/back.php
+++ application/views/layouts/back.php
@@ -77,84 +77,62 @@
 					<span class="brand-text font-weight-light">Administrator</span>
 				</a>
 				<div class="sidebar">
-					<!--
-					<div class="user-panel mt-3 pb-3 mb-3 d-flex">
-						<div class="image">
-							<img src="" class="img-circle elevation-2" alt="User Image">
-						</div>
-						<div class="info">
-							<a href="#" class="d-block">asd</a>
-						</div>
-					</div>
-					-->
 					<nav class="mt-2">
 						 <ul class="nav nav-pills nav-sidebar flex-column nav-legacy nav-compact text-sm" data-widget="treeview" role="menu" data-accordion="false">
 						 	<li class="nav-item">
-								<a href="<?php echo site_url('admin'); ?>" class="nav-link active">
+								<a href="<?php echo site_url('admin'); ?>" <?php echo $this->uri->segment(2) == 'dashboard' || $this->uri->segment(2) == '' ? 'class="nav-link active"' : ''?> class="nav-link">
 									<i class="nav-icon fa fa-tachometer"></i>
 									<p>Dashboard</p>
 								</a>
 							</li>
 							<li class="nav-item">
-								<a href="<?php echo site_url('admin/user-requests'); ?>" class="nav-link">
+								<a href="<?php echo site_url('admin/user-requests'); ?>" <?php echo $this->uri->segment(2) == 'user-requests' ? 'class="nav-link active"' : '' ?> class="nav-link">
 									<i class="nav-icon fa fa-exclamation-circle"></i>
 									<p>User Requests</p>
 								</a>
 							</li>
 							<li class="nav-item">
-								<a href="<?php echo site_url('admin/users'); ?>" class="nav-link">
+								<a href="<?php echo site_url('admin/users'); ?>" <?php echo $this->uri->segment(2) == 'users' ? 'class="nav-link active"' : ''?> class="nav-link">
 									<i class="nav-icon fa fa-users"></i>
 									<p>Users Data</p>
 								</a>
 							</li>
 							<li class="nav-item">
-								<a href="<?php echo site_url('admin/workers'); ?>" class="nav-link">
+								<a href="<?php echo site_url('admin/workers'); ?>" <?php echo $this->uri->segment(2) == 'workers' ? 'class="nav-link active"' : ''?> class="nav-link">
 									<i class="nav-icon fa fa-users"></i>
 									<p>Workers Data</p>
 								</a>
 							</li>
-							<!-- <li class="nav-item">
-								<a href="<?php echo site_url('admin/employees'); ?>" class="nav-link">
-									<i class="nav-icon fa fa-users"></i>
-									<p>Employees Data</p>
-								</a>
-							</li> -->
 							<li class="nav-item">
-								<a href="<?php echo site_url('admin/company'); ?>" class="nav-link">
+								<a href="<?php echo site_url('admin/company'); ?>" <?php echo $this->uri->segment(2) == 'company' ? 'class="nav-link active"' : ''?> class="nav-link">
 									<i class="nav-icon fa fa-building-o"></i>
 									<p>Company Profile</p>
 								</a>
 							</li>
 							<li class="nav-item">
-								<a href="<?php echo site_url('admin/sliders'); ?>" class="nav-link">
+								<a href="<?php echo site_url('admin/sliders'); ?>" <?php echo $this->uri->segment(2) == 'sliders' ? 'class="nav-link active"' : ''?> class="nav-link">
 									<i class="nav-icon fa fa-television"></i>
 									<p>Sliders</p>
 								</a>
 							</li>
-							<li class="nav-item has-treeview">
+							<li class="nav-item has-treeview <?php echo $this->uri->segment(2) == 'experiences' || $this->uri->segment(2) == 'placements' ? '"active"' : ''?>">
 								<a href="#" class="nav-link">
 									<i class="nav-icon fa fa-cog"></i>
 									<p>Settings <i class="right fa fa-angle-left"></i></p>
 								</a>
-								<ul class="nav nav-treeview">
+								<ul class="nav nav-treeview ">
 									<li class="nav-item">
-										<a href="<?php echo site_url('admin/experiences'); ?>" class="nav-link">
+										<a href="<?php echo site_url('admin/experiences'); ?>" <?php echo $this->uri->segment(2) == 'experiences' ? 'class="nav-link active"' : ''?> class="nav-link">
 											<i class="fa fa-circle-thin nav-icon"></i>
 											<p>Experiences</p>
 										</a>
 									</li>
 									<li class="nav-item">
-										<a href="<?php echo site_url('admin/placements'); ?>" class="nav-link">
+										<a href="<?php echo site_url('admin/placements'); ?>" <?php echo $this->uri->segment(2) == 'placements' ? 'class="nav-link active"' : ''?> class="nav-link">
 											<i class="fa fa-circle-thin nav-icon"></i>
 											<p>Placements</p>
 										</a>
 									</li>
-									<!-- <li class="nav-item">
-										<a href="<?php echo site_url('admin/mailer'); ?>" class="nav-link">
-											<i class="fa fa-circle-thin nav-icon"></i>
-											<p>Mailer Config</p>
-										</a>
-									</li> -->
 								</ul>
 							</li>
 							<li class="nav-item">
