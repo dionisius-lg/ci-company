1dee5fd64ca3565d1edc87cceb94b702b26eb8d1
ramdannugraha <ramdannugraha08@gmail.com>
2021-07-12 09:52:21 +0700

    untrack

---
+++ application/language/korean/english/content_lang.php
@@ -0,0 +1,142 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['front'] = [
+	'topbar' => [
+		'login'		=> 'Login',
+		'register'	=> 'Register',
+		'logout'	=> 'Logout',
+		'welcome'	=> 'Hi',
+	],
+	'navbar' => [
+		'home'		=> 'Main Page',
+		'worker'	=> 'Worker Profile',
+		'about'		=> 'About Us',
+		'contact'	=> 'Contact Us'
+		// 'gallery'	=> 'Gallery',
+		// 'dropdown'	=> 'Dropdown',
+	],
+	'footer' => [
+		'company' => [
+			'desc'	=> ' is a Recruitment and Placement Company that specializes in skilled Indonesian workforce abroad.'
+		],
+		'link' => [
+			'title'		=> 'Useful Links',
+			// 'home'		=> 'Home',
+			// 'worker'	=> 'Worker',
+			// 'gallery'	=> 'Gallery',
+			// 'dropdown'	=> 'Dropdown',
+			// 'about'		=> 'About Us',
+			// 'contact'	=> 'Contact Us'
+		],
+		'contact' => [
+			'title'		=> 'Contact Us',
+			'phone'		=> 'Phone',
+			'email'		=> 'Email'
+		],
+	],
+	'section_title' => [
+		'advantages'	=> 'Our Advantages',
+		'clients'		=> 'Our Clients',
+		'news'			=> 'News'
+	],
+	'page_contact' => [
+		'info' => [
+			'title'		=> 'Contact Info',
+			'location'	=> 'Location',
+			'email'		=> 'Email',
+			'phone'		=> 'Cell Phone/ Whatsapp Number'
+		],
+		'message' => [
+			'title'		=> 'Get In Touch',
+			'name'		=> 'Your Name',
+			'email'		=> 'Your Email',
+			'subject'	=> 'Subject',
+			'message'	=> 'Messages',
+			'send'		=> 'Send Message'
+		],
+	],
+	'page_login' => [
+		'intro'		=> 'Login to start your session',
+		'username'	=> 'Username or email',
+		'password'	=> 'Password',
+		'submit'	=> 'Login'
+	],
+	'page_register' => [
+		'intro'				=> 'Register a new membership',
+		'fullname'			=> 'Fullname',
+		'email'				=> 'Email',
+		'register_as'		=> 'Register As',
+		'agency_location'	=> 'Agency Location',
+		'company'			=> 'Company',
+		'submit'			=> 'Register'
+	],
+	'page_worker' => [
+		'filter' => 'Filter',
+		'result' => 'Result',
+		'no_result' => 'No Result Found',
+		'attachment' => 'Attachment',
+		'profile' => 'Personal Data',
+		'family_background' => 'Family Background',
+		'skills' => 'Skills',
+		'contact' => 'Contact',
+		'others' => 'Others',
+		'worker_data' => [
+			'ref_number'			=> 'Ref. Number',
+			'fullname'				=> 'Fullname',
+			'gender'				=> 'Gender',
+			'birth_place'			=> 'Birth Place',
+			'birth_date'			=> 'Birth Date',
+			'age'					=> 'Age',
+			'marital_status'		=> 'Marital Status',
+			'religion'				=> 'Religion',
+			'email'					=> 'Email',
+			'phone'					=> 'Phone',
+			'address'				=> 'Address',
+			'description'			=> 'Description',
+			'placement'				=> 'Placement Now',
+			'skill_experience'		=> 'Skill Experience',
+			'work_experience'		=> 'Work Experience',
+			'ready_placement'		=> 'Ready For Placement',
+			'last_education'		=> 'Last Education',
+			'character_evaluation'	=> 'Personal Character Evaluation',
+			'spouse_name'			=> 'Spouse Name',
+			'spouse_occupation'		=> 'Spouse Occupation',
+			'children'				=> 'Children',
+			'children_age'			=> 'Children Age',
+			'father_name'			=> 'Father Name',
+			'father_occupation'		=> 'Father Occupation',
+			'mother_name'			=> 'Mother Name',
+			'mother_occupation'		=> 'Mother Occupation',
+			'language_ability'		=> 'Language Ability',
+			'cooking_ability'		=> 'Cooking Ability',
+		],
+		'button' => [
+			'search'		=> 'Search',
+			'view_detail'	=> 'View Detail',
+			'view_avatar'	=> 'Zoom Photo',
+			'download_data'	=> 'Biodata',
+			'play_video'	=> 'Video'
+		],
+	],
+	'button' => [
+		'readmore'		=> 'Read More',
+		'contact'		=> 'Contact Us',
+		'send_message'	=> 'Send Message'
+	],
+];
+
+$lang['back'] = [];
+
+$lang['message'] = [
+	'error' => [
+		'default'	=> 'An error occurred, please try again',
+		'auth'		=> 'Please login first',
+		'login'		=> 'Invalid login credentials',
+		'captcha'	=> 'Captcha is required'
+	],
+	'success' => [
+		'default'		=> '',
+		'register'		=> 'We are currently processing your request, please wait for confirmation via email',
+		'verification'	=> 'Verification has been sent, please check your email'
+	],
+];
