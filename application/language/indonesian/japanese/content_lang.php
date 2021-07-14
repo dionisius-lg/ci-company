6176a94b4dfc0363dbe7e0eeb7bd63c69798f88f
ramdannugraha <ramdannugraha08@gmail.com>
2021-07-14 14:51:03 +0700

    update our adventage

---
+++ application/language/indonesian/japanese/content_lang.php
@@ -0,0 +1,142 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['front'] = [
+	'topbar' => [
+		'login'		=> 'ログインする',
+		'register'	=> '登録',
+		'logout'	=> 'ログアウト',
+		'welcome'	=> 'こんにちは',
+	],
+	'navbar' => [
+		'home'		=> 'メインページ',
+		'worker'	=> '労働者',
+		'gallery'	=> 'ギャラリー',
+		'dropdown'	=> '落ちる',
+		'about'		=> '私たちに関しては',
+		'contact'	=> 'お問い合わせ'
+	],
+	'footer' => [
+		'company' => [
+			'desc'	=> ' は、海外で熟練したインドネシア人労働力を専門とする採用および配置会社です。'
+		],
+		'link' => [
+			'title'		=> '便利なリンク',
+			// 'home'		=> '家',
+			// 'worker'	=> '従業員',
+			// 'gallery'	=> 'ギャラリー',
+			// 'dropdown'	=> '落ちる',
+			// 'about'		=> '私たちに関しては',
+			// 'contact'	=> 'お問い合わせ'
+		],
+		'contact' => [
+			'title'		=> 'お問い合わせ',
+			'phone'		=> '電話',
+			'email'		=> '電子メイル'
+		],
+	],
+	'section_title' => [
+		'advantages'	=> '支店とパートナー',
+		'clients'		=> '我々の顧客',
+		'news'			=> 'ニュース'
+	],
+	'page_contact' => [
+		'info' => [
+			'title'		=> '連絡先情報',
+			'location'	=> 'ロケーション',
+			'email'		=> '電子メイル',
+			'phone'		=> '連絡先番号'
+		],
+		'message' => [
+			'title'		=> '接続を維持',
+			'name'		=> 'あなたの名前',
+			'email'		=> 'あなたのメール',
+			'subject'	=> '件名',
+			'message'	=> 'メッセージ',
+			'send'		=> 'メッセージを送る'
+		],
+	],
+	'page_login' => [
+		'intro'		=> 'ログインしてセッションを開始します',
+		'username'	=> 'ユーザー名または電子メール',
+		'password'	=> 'パスワード',
+		'submit'	=> 'ログイン'
+	],
+	'page_register' => [
+		'intro'				=> '新しいメンバーシップを登録する',
+		'fullname'			=> 'フルネーム',
+		'email'				=> '電子メイル',
+		'register_as'		=> 'として登録',
+		'agency_location'	=> '代理店の場所',
+		'company'			=> '会社',
+		'submit'			=> '登録'
+	],
+	'page_worker' => [
+		'filter' => 'フィルタ',
+		'result' => '結果',
+		'no_result' => '結果が見つかりません',
+		'attachment' => 'アタッチメント',
+		'profile' => '個人データ',
+		'family_background' => '家族の背景',
+		'skills' => 'スキル',
+		'contact' => '連絡先データ',
+		'others' => 'その他',
+		'worker_data' => [
+			'ref_number'			=> '参照番号',
+			'fullname'				=> 'フルネーム',
+			'gender'				=> '性別',
+			'birth_place'			=> '出生地',
+			'birth_date'			=> '誕生日',
+			'age'					=> '年齢',
+			'marital_status'		=> '配偶者の有無',
+			'religion'				=> '宗教',
+			'email'					=> '電子メイル',
+			'phone'					=> '電話',
+			'address'				=> '住所',
+			'description'			=> '説明',
+			'placement'				=> '今すぐ配置',
+			'skill_experience'		=> 'スキル経験',
+			'work_experience'		=> '実務経験',
+			'ready_placement'		=> '配置の準備ができました',
+			'last_education'		=> '最後の教育',
+			'character_evaluation'	=> '個人的な性格評価',
+			'spouse_name'			=> '配偶者の名前',
+			'spouse_occupation'		=> '配偶者の職業',
+			'children'				=> '子供達',
+			'children_age'			=> '子供の年齢',
+			'father_name'			=> '父の名前',
+			'father_occupation'		=> '父の職業',
+			'mother_name'			=> '母の名前',
+			'mother_occupation'		=> '母の職業',
+			'language_ability'		=> '言語能力',
+			'cooking_ability'		=> '料理能力',
+		],
+		'button' => [
+			'search'		=> '探す',
+			'view_detail'	=> '詳細を表示',
+			'view_avatar'	=> '写真をズーム',
+			'download_data'	=> 'バイオデータ',
+			'play_video'	=> 'ビデオ'
+		],
+	],
+	'button' => [
+		'readmore'		=> '続きを見る',
+		'contact'		=> 'お問い合わせ',
+		'send_message'	=> 'メッセージを送る'
+	],
+];
+
+$lang['back'] = [];
+
+$lang['message'] = [
+	'error' => [
+		'default'	=> 'エラーが発生しました, もう一度やり直してください',
+		'auth'		=> '最初にログインしてください',
+		'login'		=> 'キャプチャが必要です',
+		'captcha'	=> '無効なログイン資格情報'
+	],
+	'success' => [
+		'default'		=> '',
+		'register'		=> '現在、リクエストを処理しています。メールでの確認をお待ちください',
+		'verification'	=> '確認が送信されました, メールを確認してください'
+	],
+];
--- application/views/templates/front/Element/section_advantage.php
+++ application/views/templates/front/Element/section_advantage.php
@@ -1,99 +1,205 @@
 <section id="services" class="section-bg">
+
 	<div class="container">
+
 		<div class="section-title">
+
 			<h2><?php echo $this->lang->line('front')['section_title']['advantages']; ?></h2>
+
 		</div>
+
+
 
 		<div class="row">
+
 			<div class="col-lg-4 col-md-6 mt-md-0 mb-3">
+
 				<div class="item text-center match-height">
-					<i class="fa fa-check-square-o"></i>
+
+					
+
 					<?php if (siteLang()['key'] == 'en') { ?>
-						<h4><a href="#">PT. AMALIA ROZIKIN JAYA</a></h4>
-						<p>KENDAL Branch JL. Lanji RT.002/001, Lanji, Patebon, Kendal, Central Java, Indonesia.</p>
+
+						<h4>PT. AMALIA ROZIKIN JAYA</h4>
+
+						<h4>KENDAL Branch</h4> 
+						<p>JL. Lanji RT.002/001, Lanji, Patebon, Kendal, Central Java, Indonesia.</p>
+
 					<?php } ?>
+
+
 
 					<?php if (siteLang()['key'] == 'id') { ?>
-						<h4><a href="#">PT. AMALIA ROZIKIN JAYA</a></h4>
-						<p>Cabang KENDAL Jalan Lanji RT.002/001, Lanji, Patebon, Kab. Kendal, Jawa Tengah</p>
+
+						<h4>PT. AMALIA ROZIKIN JAYA</h4>
+
+						<h4>Cabang KENDAL </h4>
+<p>Jalan Lanji RT.002/001, Lanji, Patebon, Kab. Kendal, Jawa Tengah</p>
+
 					<?php } ?>
 
+
+
 					<?php if (siteLang()['key'] == 'ja') { ?>
-						<h4><a href="#">PT. AMALIA ROZIKIN JAYA</a></h4>
-						<p>KENDAL 支店 JL. Lanji RT.002/001, Lanji, Patebon, Kendal, インドネシア中部ジャワ。</p>
+
+						<h4>PT. AMALIA ROZIKIN JAYA</h4>
+
+						<h4>KENDAL 支店 </h4>
+<p>JL. Lanji RT.002/001, Lanji, Patebon, Kendal, インドネシア中部ジャワ。</p>
+
 					<?php } ?>
+
+
 
 					<?php if (siteLang()['key'] == 'ko') { ?>
-						<h4><a href="#">PT. AMALIA ROZIKIN JAYA</a></h4>
-						<p>KENDAL 지점 JL. Lanji RT.002/001, Lanji, Patebon, Kendal, 중부 자바，인도네시아</p>
+
+						<h4>PT. AMALIA ROZIKIN JAYA</h4>
+
+						<h4>KENDAL 지점 </h4>
+<p>JL. Lanji RT.002/001, Lanji, Patebon, Kendal, 중부 자바，인도네시아</p>
+
 					<?php } ?>
+
+
 
 					<?php if (siteLang()['key'] == 'zh-TW') { ?>
-						<h4><a href="#">PT. AMALIA ROZIKIN JAYA</a></h4>
-						<p>KENDAL 分公司 JL. Lanji RT.002/001, Lanji, Patebon, Kendal, 中爪哇，印尼。</p>
+
+						<h4>PT. AMALIA ROZIKIN JAYA</h4>
+
+						<h4>KENDAL 分公司</h4>
+<p> JL. Lanji RT.002/001, Lanji, Patebon, Kendal, 中爪哇，印尼。</p>
+
 					<?php } ?>
+
 				</div>
+
 			</div>
+
 			<div class="col-lg-4 col-md-6 mt-md-0 mb-3">
+
 				<div class="item text-center match-height">
-					<i class="fa fa-check-square-o"></i>
 
 					<?php if (siteLang()['key'] == 'en') { ?>
-						<h4><a href="#">BERSAMA KIAN SUKSES (BKS) Skill Training Center</a></h4>
-						<p>This is example with english language. Magnam dolores commodi suscipit.</p>
+
+						<h4>BERSAMA KIAN SUKSES (BKS) </h4>
+<h4>Skill Training Center</h4>
+
+						<p>JL. Letjen Suprapto No.2, Ngijo, Tasikmadu, Karanganyar, Solo, Central Java, Indonesia.
+
+Tel: 0271-6497349.</p>
+
 					<?php } ?>
+
+
 
 					<?php if (siteLang()['key'] == 'id') { ?>
-						<h4><a href="#">LPKS BLKLN BERSAMA KIAN SUKSES</a></h4>
-						<p>Ini adalah contoh dalam bahasa Indonesia. Magnam dolores commodi suscipit.</p>
+
+						<h4>LPKS BLKLN BERSAMA KIAN SUKSES</h4>
+
+						<p>Jalan Letjen Suprapto No.2, Ngijo, Tasikmadu, Karanganyar, Solo, Jawa Tengah
+
+Tel: 0271-6497349.</p>
+
 					<?php } ?>
 
+
+
 					<?php if (siteLang()['key'] == 'ja') { ?>
-						<h4><a href="#">BERSAMA KIAN SUKSES (BKS) スキルトレーニングセンター</a></h4>
+
+						<h4>BERSAMA KIAN SUKSES (BKS)</h4>
+<h4>スキルトレーニングセンター</h4>
+
 						<p>JL. Letjen Suprapto No.2, Ngijo, Tasikmadu, Karanganyar, ソロ、中部ジャワ、インドネシア。Tel: 0271-6497349</p>
+
 					<?php } ?>
+
+
 
 					<?php if (siteLang()['key'] == 'ko') { ?>
-						<h4><a href="#">BERSAMA KIAN SUKSES (BKS) 기술 훈련 센터</a></h4>
+
+						<h4>BERSAMA KIAN SUKSES (BKS)</h4>
+<h4> 기술 훈련 센터</h4>
+
 						<p>JL. Letjen Suprapto No.2, Ngijo, Tasikmadu, Karanganyar, 솔로, 중부 자바, 인도네시아。Tel: 0271-6497349</p>
+
 					<?php } ?>
 
+
+
 					<?php if (siteLang()['key'] == 'zh-TW') { ?>
-						<h4><a href="#">BERSAMA KIAN SUKSES (BKS) 技能訓練中心</a></h4>
+
+						<h4>BERSAMA KIAN SUKSES (BKS) </h4>
+<h4>技能訓練中心</h4>
+
 						<p>JL. Letjen Suprapto No.2, Ngijo, Tasikmadu, Karanganyar, 梭羅，中爪哇，印尼Tel: 0271-6497349</p>
+
 					<?php } ?>
+
 				</div>
+
 			</div>
+
 			<div class="col-lg-4 col-md-6 mt-md-0 mb-3">
+
 				<div class="item text-center match-height">
-					<i class="fa fa-check-square-o"></i>
+
+
 
 					<?php if (siteLang()['key'] == 'en') { ?>
-						<h4><a href="#">CANA PLACEMENT SERVICES LIMITED</a></h4>
+
+						<h4>CANA PLACEMENT SERVICES LIMITED</h4>
+
 						<p>Shop 88,G/F, Eastern Plaza. Koway Court, 111 Chai Wan Road, Hong Kong.</p>
+
 					<?php } ?>
+
+
 
 					<?php if (siteLang()['key'] == 'id') { ?>
-						<h4><a href="#">CANA PLACEMENT SERVICES LIMITED</a></h4>
+
+						<h4>CANA PLACEMENT SERVICES LIMITED</h4>
+
 						<p>Shop 88,G/F, Eastern Plaza. Koway Court, 111 Chai Wan Road, Hong Kong.</p>
+
 					<?php } ?>
+
+
 
 					<?php if (siteLang()['key'] == 'ja') { ?>
-						<h4><a href="#">CANA PLACEMENT SERVICES LIMITED</a></h4>
+
+						<h4>CANA PLACEMENT SERVICES LIMITED</h4>
+
 						<p>Shop 88,G/F, Eastern Plaza. Koway Court, 111 Chai Wan Road, Hong Kong.</p>
+
 					<?php } ?>
+
+
 
 					<?php if (siteLang()['key'] == 'ko') { ?>
-						<h4><a href="#">CANA PLACEMENT SERVICES LIMITED</a></h4>
+
+						<h4>CANA PLACEMENT SERVICES LIMITED</h4>
+
 						<p>Shop 88,G/F, Eastern Plaza. Koway Court, 111 Chai Wan Road, Hong Kong.</p>
+
 					<?php } ?>
 
+
+
 					<?php if (siteLang()['key'] == 'zh-TW') { ?>
-						<h4><a href="#">CANA PLACEMENT SERVICES LIMITED</a></h4>
+
+						<h4>CANA PLACEMENT SERVICES LIMITED</h4>
+
 						<p>Shop 88,G/F, Eastern Plaza. Koway Court, 111 Chai Wan Road, Hong Kong.</p>
+
 					<?php } ?>
+
 				</div>
+
 			</div>
+
 		</div>
+
 	</div>
+
 </section>
+
